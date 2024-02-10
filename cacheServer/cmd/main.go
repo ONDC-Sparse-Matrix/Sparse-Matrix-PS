b45f012465501b5a256f465508679e9055061ccf
sukhman-sukh <sukhmansinghsaluja@gmail.com>
2024-02-10 23:26:08 +0530

    feat: Central-Server and system Architecture implemented

--- cacheServer/cmd/main.go
+++ cacheServer/cmd/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"cache-server/utils"
 	"fmt"
-
 	"github.com/gofiber/fiber/v2"
+	"github.com/patrickmn/go-cache"
+	// "net/http"
+	"time"
 )
 
 func Initialize() {
@@ -13,7 +15,9 @@ func Initialize() {
 
 func main() {
 	Initialize()
-	app := fiber.New()
+	cache1 := cache.New(48*time.Hour, 48*time.Hour)
+	config := fiber.Config{ServerHeader: "Cache Server", Prefork: true}
+	app := fiber.New(config)
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
@@ -22,9 +26,32 @@ func main() {
 	app.Get("/pincode/:pincode?", func(c *fiber.Ctx) error {
 		pincode := c.Params("pincode")
 		fmt.Println("pincode", pincode)
-		jsonData := utils.CheckPincode(pincode, c)
-		return c.JSON(jsonData)
+		jsonData := utils.CheckPincode(pincode, c, cache1)
+		return c.SendString(string(jsonData))
 	})
+	type UpdateRequestBody struct {
+		PincodeList []string `json:"pincodeList"`
+	}
+
+	app.Post("/update/", func(c *fiber.Ctx) error {
+		// Parse the request body into a struct
+		var requestBody UpdateRequestBody
+		if err := c.BodyParser(&requestBody); err != nil {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid request body"})
+		}
+		utils.CheckCache(requestBody.PincodeList, cache1)
+		// Process the data as needed
+		// response := fiber.Map{
+		// 	"message": "Received POST request",
+		// 	"name":    requestBody.Name,
+		// 	"email":   requestBody.Email,
+		// }
+
+		// Send a JSON response
+		return c.SendString("Recieved Response")
+	})
+
+	// app.Get
 
 	app.Listen(":3000")
 }
--- cacheServer/utils/checkPincode.go
+++ cacheServer/utils/checkPincode.go
@@ -6,9 +6,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 
 	// "net/url"
-	"strconv"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -16,8 +16,8 @@ import (
 )
 
 const (
-	DefaultExpiration = 48 * time.Hour
-	PurgeInterval     = 48 * time.Hour
+	DefaultExpiration = 24 * time.Hour
+	PurgeInterval     = 64 * time.Hour
 )
 
 // JSON Format to recieve in body from database
@@ -31,65 +31,102 @@ const (
 // {pincode : 221144, merchantList : ["merchant1", "merchant2", "merchant3"]},
 // }
 
-func cacheMiss(pincode string, c *cache.Cache) []types.PincodeInfo {
-	pincodeInt, _ := strconv.Atoi(pincode)
-	for i := pincodeInt - 5; i <= pincodeInt+5; i++ {
-		fmt.Println("Cache miss")
-		var pincodeInfo types.PincodeInfo
-		baseUrl := fmt.Sprintf("http://%s:%s/pincode/%s", types.Server.Host, types.Server.Port, pincode)
-		resp, err := http.Get(baseUrl)
-		body, err := ioutil.ReadAll(resp.Body)
+func cacheMiss(pincode string, c *cache.Cache) types.SendPincodeInfo {
+	// pincodeInt, _ := strconv.Atoi(pincode)
+	// for i := pincodeInt - 5; i <= pincodeInt+5; i++ {
+	fmt.Println("Cache miss")
+	var pincodeInfo types.PincodeInfo
+	var newPincodeInfoList []types.PincodeInfo
+	// var currentPincodeInfo types.PincodeInfo
+	var sendPincodeInfo3 types.ParseJson
+	var sendPincodeInfo types.SendPincodeInfo
+	var sendPincodeInfo2 types.SendPincodeInfo
 
-		err = json.Unmarshal(body, &(types.PincodeInfoList))
-		if err != nil {
-			fmt.Println("Error unmarshalling JSON:", err)
-			return nil
-		}
-
-		// Access each element in the array
-		for _, PincodeInfo := range types.PincodeInfoList {
-			var cacheInfo types.CacheInfo
-
-			if pincode == PincodeInfo.Pincode {
-				pincodeInfo.MerchantList = PincodeInfo.MerchantList
-				pincodeInfo.Pincode = PincodeInfo.Pincode
-				fmt.Println("asdasd", pincodeInfo)
-			}
-			cacheInfo.LastModified = time.Now().UTC()
-			cacheInfo.MerchantList = PincodeInfo.MerchantList
-			c.Set(pincode, cacheInfo, cache.DefaultExpiration)
-			fmt.Printf("Pincode: %s, Merchants: %v\n", PincodeInfo.Pincode, PincodeInfo.MerchantList)
-		}
-		fmt.Println(pincodeInfo)
-		types.PincodeInfoList = append(types.PincodeInfoList, pincodeInfo)
+	baseUrl := fmt.Sprintf("http://%s:%s/merchants/%s", types.Server.Host, types.Server.Port, pincode)
+	fmt.Println(baseUrl)
+	resp, err := http.Get(baseUrl)
+	body, err := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
+	err = json.Unmarshal(body, &(sendPincodeInfo3))
+	if err != nil {
+		fmt.Println("Error unmarshalling JSON:", err)
+		// return nil
 	}
-	return types.PincodeInfoList
+	sendPincodeInfo = sendPincodeInfo3.SendPincodeInfo2
+
+	currentPincodeInfo := sendPincodeInfo.Current
+	var startIndex, endIndex, count int
+	startIndex = 20
+	endIndex = 40
+	fmt.Println("CAche ================== ", sendPincodeInfo.Cache)
+	// Access each element in the array
+	count = 0
+	for _, PincodeInfo := range sendPincodeInfo.Cache {
+		var cacheInfo types.CacheInfo
+
+		pincodeInfo.MerchantList = PincodeInfo.MerchantList
+		pincodeInfo.Pincode = PincodeInfo.Pincode
+		fmt.Println("asdasd ", pincodeInfo, "\n\n")
+		count++
+		if count >= startIndex && count <= endIndex {
+			newPincodeInfoList = append(newPincodeInfoList, pincodeInfo)
+		}
+		cacheInfo.LastModified = time.Now().UTC()
+		cacheInfo.MerchantList = PincodeInfo.MerchantList
+		c.Set(pincode, cacheInfo, cache.DefaultExpiration)
+		// fmt.Printf("Pincode: %s, Merchants: %v\n", PincodeInfo.Pincode, PincodeInfo.MerchantList)
+	}
+	fmt.Println(newPincodeInfoList)
+	sendPincodeInfo2.Current = currentPincodeInfo
+	sendPincodeInfo2.Cache = newPincodeInfoList
+	// types.PincodeInfoList = append(types.PincodeInfoList, pincodeInfo)
+	// }
+	return sendPincodeInfo2
 }
 
-func CheckPincode(pincode string, ctx *fiber.Ctx) []byte {
+func CheckPincode(pincode string, ctx *fiber.Ctx, c *cache.Cache) []byte {
 	types.PincodeInfoList = types.PincodeInfoList[:0]
-	c := cache.New(48*time.Hour, 48*time.Hour)
-
+	var sendPincodeInfo types.SendPincodeInfo
+	var pincodeInfoList []types.PincodeInfo
+	var pincodeInfoo types.PincodeInfo
 	if x, found := c.Get(pincode); found {
 		fmt.Println("Cache hit")
 		var cacheInfo types.CacheInfo
 		cacheInfo = x.(types.CacheInfo)
 
-		// lastModified := cacheInfo.LastModified
-		// baseUrl := fmt.Sprintf("http://%s:%s/checkLastModified?pincode=%s&date=%s", types.Server.Host, types.Server.Port, pincode, lastModified.String())
+		sendPincodeInfo.Current.Pincode = pincode
+		sendPincodeInfo.Current.MerchantList = cacheInfo.MerchantList
 
-		// resp, _ := http.Get(baseUrl)
-		// body, _ := ioutil.ReadAll(resp.Body) // {status: 200}
-		// if string(body) != "200" {
-		// 	types.PincodeInfoList = cacheMiss(pincode, c)
-		// }
+		// Fill cache server
+		pincodeInt, _ := strconv.Atoi(pincode)
+		for i := pincodeInt - 10; i <= pincodeInt+10; i++ {
+			if x, found := c.Get(strconv.Itoa(i)); found {
+				fmt.Println("Cache hit")
+				var cacheInfo types.CacheInfo
+				cacheInfo = x.(types.CacheInfo)
+				pincodeInfoo.Pincode = pincode
+				pincodeInfoo.MerchantList = cacheInfo.MerchantList
 
-		types.PincodeInfoList[0].Pincode = pincode
-		types.PincodeInfoList[0].MerchantList = cacheInfo.MerchantList
+				pincodeInfoList = append(pincodeInfoList, pincodeInfoo)
+			} else {
+				continue
+			}
+		}
+		sendPincodeInfo.Cache = pincodeInfoList
 	} else {
-		types.PincodeInfoList = cacheMiss(pincode, c)
+		sendPincodeInfo = cacheMiss(pincode, c)
 	}
-
-	jsonData, _ := json.Marshal(types.PincodeInfoList)
+	jsonData, _ := json.Marshal(sendPincodeInfo)
 	return jsonData
+}
+
+func CheckCache(pincodeList []string, c *cache.Cache) {
+
+	for _, pincode := range pincodeList {
+		if x, found := c.Get(pincode); found {
+			fmt.Println("Cache hit now update")
+			cacheInfo := x.(types.CacheInfo)
+			cacheInfo.MerchantList = append(cacheInfo.MerchantList)
+		}
+	}
 }
